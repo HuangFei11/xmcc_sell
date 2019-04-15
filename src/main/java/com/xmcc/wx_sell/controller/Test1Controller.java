a26f19651afdba325a621397b1039164aff66cfb
xmcc <H1355032122@163.com>
2019-04-15 14:54:16 +0800

    第二次提交：测试

---
+++ src/main/java/com/xmcc/wx_sell/controller/Test1Controller.java
@@ -0,0 +1,10 @@
+package com.xmcc.wx_sell.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Test1Controller {
+    public void  test(){
+
+    }
+}
