4cb44153989960182cb7503160e05bd8edf6b37e
xmcc <H1355032122@163.com>
2019-04-15 14:45:35 +0800

    第一次提交：测试1

---
+++ src/main/java/com/xmcc/wx_sell/WxSellApplication.java
@@ -0,0 +1,16 @@
+package com.xmcc.wx_sell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WxSellApplication {
+/**
+ * springboot的启动类
+ *
+ */
+	public static void main(String[] args) {
+		SpringApplication.run(WxSellApplication.class, args);
+	}
+
+}
